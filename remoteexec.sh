83ebf03da123b8e423d87406e40c8b712344d0bf
Jose-Alvaro-Cedeno-Panchana <Jose.Alvaro.Cedeno.Panchana@ocean.ibm.com>
2022-03-30 00:38:01 +0200

    30/03/2022

--- remoteexec.sh
+++ remoteexec.sh
@@ -1,16 +1,11 @@
 #! /bin/bash
 servidores=$(cat /tmp/remotes.txt)
-
-<<<<<<< HEAD
 SSHPASS=Dopracau2000lopo
-=======
-SSHPASS=w6EjLf88eBh4UOJ
->>>>>>> 282f4bf5f988b13e11305979656d4b45d697c572
 export SSHPASS
 
 for host in $servidores; do 
 	sshpass -e ssh-copy-id $host
-	scp /home/esy9d7l1/Alvaro/Desarrollo/Bash/sudo_itss_preg4.sh  $host:/tmp/
+	scp /tmp/sudo_itss_preg4.sh  $host:/tmp/
 done
 
 for host in $servidores; do
