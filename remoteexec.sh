5861e3b2c5a0bc25749086ff9fded8e566a164a2
Jose-Alvaro-Cedeno-Panchana <Jose.Alvaro.Cedeno.Panchana@ocean.ibm.com>
2022-04-01 21:59:28 +0200

    01/04/2022

--- remoteexec.sh
+++ remoteexec.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
+. tsocks on
 servidores=$(cat /tmp/remotes.txt)
-SSHPASS=Dopracau2000lopo
+
+SSHPASS=BailaconLOP2020
 export SSHPASS
+
+echo $SSHPASS
 
 for host in $servidores; do 
 	sshpass -e ssh-copy-id $host
@@ -9,9 +13,10 @@ for host in $servidores; do
 done
 
 for host in $servidores; do
-	ssh $host 'sudo /tmp/sudo_itss_preg4.sh;
+	ssh -t $host 'sudo /tmp/sudo_itss_preg4.sh;
 	rm /tmp/sudo_itss_preg4.sh &> /dev/null;
 	user=$(logname);
 	rm /home/${user^^}/.ssh/authorized_keys &> /dev/null;
 	rm /home/${user,,}/.ssh/authorized_keys &> /dev/null'
 done
+sleep 10s
